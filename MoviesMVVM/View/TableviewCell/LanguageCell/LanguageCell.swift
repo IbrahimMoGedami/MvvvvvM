139b1934222f0472a90da5473431f6c197460d97
Ibrahim Gedami <hemagedamy@gmail.com>
2022-07-28 06:01:42 +0200

    passed

---
+++ MoviesMVVM/View/TableviewCell/LanguageCell/LanguageCell.swift
@@ -0,0 +1,18 @@
+//
+//  LanguageCell.swift
+//  MoviesMVVM
+//
+//  Created by Mani on 2/5/22.
+//
+
+import UIKit
+
+class LanguageCell: UITableViewCell {
+    @IBOutlet weak var name: UILabel!
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    
+}
